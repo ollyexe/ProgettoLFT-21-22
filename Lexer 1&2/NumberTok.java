b515cfae51493dc196d673e90b1897ccdcae4879
paradigmastic69 <95639981+paradigmastic69@users.noreply.github.com>
2022-02-15 11:49:47 +0100

    Delete Lexer 1&2 directory

--- Lexer 1&2/NumberTok.java
+++
@@ -1,9 +0,0 @@
-public class NumberTok extends Token {
-    int n =0 ;
-    public NumberTok(int tag, int n) {
-        super(tag);
-        this.n = n;}
-    public String toString(){
-        return "<" + tag + ", " + n + ">";
-    }
-}
