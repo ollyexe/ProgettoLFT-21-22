7cd3669b6ac16c49ab51aff93999e0ac74fd57de
paradigmastic69 <95639981+paradigmastic69@users.noreply.github.com>
2022-02-15 12:03:39 +0100

    Update Parser.java

--- Valutatore/Parser.java
+++ Valutatore/Parser.java
@@ -1,9 +1,4 @@
-/**
- * Membri Gruppo:
- * Oliviu Gratii
- * Alessio Di Dio
- * Alessio Mino Chimento 
- */
+
 import java.io.*;
 public class Parser {
     // analizzatore sintattico che controlla se la successione di token, 
